8891ebf77bab3aba9dc0e0574f2d97e049429d1f
Attraya Das <attrayaghoshdas@gmail.com>
2023-05-23 01:02:26 +0530

    Integrate Swagger

--- src/main/java/com/attraya/restcontroller/TouristController.java
+++ src/main/java/com/attraya/restcontroller/TouristController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.attraya.model.Tourist;
 import com.attraya.service.ITouristMgmtService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/api/tourist")
 public class TouristController {
@@ -26,30 +28,35 @@ public class TouristController {
 	private ITouristMgmtService service;
 
 	@PostMapping("/register")
+	@ApiOperation("for tourist's registration")
 	public ResponseEntity<String> enrollTourist(@RequestBody Tourist tourist) {
 		String resultMsg = service.registerTourist(tourist);
 		return new ResponseEntity<String>(resultMsg, HttpStatus.OK);
 	}
 
 	@GetMapping("/findAll")
+	@ApiOperation("for getting all tourist's information")
 	public ResponseEntity<?> displayTouristDetails() {
 		List<Tourist> list = service.fetchAllTourist();
 		return new ResponseEntity<List<Tourist>>(list, HttpStatus.OK);
 	}
 
 	@GetMapping("/find/{id}")
+	@ApiOperation("for getting specific tourist's information")
 	public ResponseEntity<?> displayTouristById(@PathVariable("id") Integer id) {
 		Tourist tourist = service.fetchTouristById(id);
 		return new ResponseEntity<Tourist>(tourist, HttpStatus.OK);
 	}
 
 	@PutMapping("/modify")
+	@ApiOperation("for modifying tourist's information")
 	public ResponseEntity<String> modifytourist(@RequestBody Tourist tourist) {
 		String msg = service.updateTouristByDetails(tourist);
 		return new ResponseEntity<String>(msg, HttpStatus.OK);
 	}
 
 	@PatchMapping("/budgetModify/{id}/{hike}")
+	@ApiOperation("for modifying budget of tourists")
 	public ResponseEntity<String> modifytouristBudgetById(@PathVariable("id") Integer id,
 			@PathVariable("hike") Float hikeAmt) {
 		String msg = service.updateTouristById(id, hikeAmt);
@@ -57,6 +64,7 @@ public class TouristController {
 	}
 
 	@DeleteMapping("/delete/{id}")
+	@ApiOperation("for deleting tourist's information")
 	public ResponseEntity<String> removeTouristById(@PathVariable("id") Integer id) {
 		String msg = service.deleteTouristById(id);
 		return new ResponseEntity<String>(msg, HttpStatus.OK);
